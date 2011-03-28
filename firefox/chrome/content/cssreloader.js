f3a4082f812dcd71b66523199f525f63aabb959b
Kenneth Auchenberg <kenneth@auchenberg.dk>
2011-03-28 13:24:03 +0200

    To support firefox 4, I changed loop since queryselectorAll now returns a NodeList instead of an array.

--- firefox/chrome/content/cssreloader.js
+++ firefox/chrome/content/cssreloader.js
@@ -3,10 +3,11 @@ CSSreloader = function() {
 
 	function reload() {
 		var elements = window.content.document.querySelectorAll('link[rel=stylesheet][href]');
-		elements.forEach(function(element, index, array) {
+		for (var i = 0; i < elements.length; ++i) {
+			var element = elements[i];
 			var h = element.href.replace(/[?&]cssReloader=([^&$]*)/,'');	
-			element.href = h + (h.indexOf('?')>=0?'&':'?') + 'cssReloader=' + (new Date().valueOf());
-		});
+			element.href = h + (h.indexOf('?')>=0?'&':'?') + 'cssReloader=' + (new Date().valueOf());		
+		}
 	}
 	
 	return {
