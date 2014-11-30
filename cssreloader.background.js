b23bdbf1cee94d766483b76119b02dad8eb8b5c8
Kenneth Auchenberg <kenneth@auchenberg.dk>
2014-11-30 09:21:25 +0100

    Formatting.

--- cssreloader.background.js
+++ cssreloader.background.js
@@ -1,50 +1,54 @@
 (function () {
 
-  var storageKey = "shortcutOptions";
-  var defaultShortcutOptions = {
-    "keyIdentifier" :  'F9',
-    "altKeySelected": false,
-    "controlKeySelected" : false,
-    "shiftKeySelected": false
-  };
+    var storageKey = "shortcutOptions";
+    var defaultShortcutOptions = {
+        "keyIdentifier"      : 'F9',
+        "altKeySelected"     : false,
+        "controlKeySelected" : false,
+        "shiftKeySelected"   : false
+    };
   
 	function initialize() {
-		chrome.contextMenus.create({"title": "Reload CSS", "type": "normal", "onclick": onContextMenuClicked});
-	  chrome.extension.onRequest.addListener(onExtensionRequest);
-  }
+	   chrome.contextMenus.create({"title": "Reload CSS", "type": "normal", "onclick": onContextMenuClicked});
+	   chrome.extension.onRequest.addListener(onExtensionRequest);
+    }
 
-  function onExtensionRequest(request, sender, callback) {
-    if (request.action == 'getSettings') {
-        callback(getSettings());
+    function onExtensionRequest(request, sender, callback) {
+        
+        if (request.action == 'getSettings') {
+            callback(getSettings());
+        }
+        
+        if (request.action == 'saveSettings') {
+            saveSettings(request.data);
+        }
+
     }
-    if (request.action == 'saveSettings') {
-        saveSettings(request.data);
-    }
-  }
 	
 	function onContextMenuClicked(info, tab) {
 		chrome.tabs.sendRequest(tab.id, {action: "reload"});	
 	}
 
-  function saveSettings(settings) {
-    if(settings) {
-      localStorage[storageKey] = JSON.stringify(settings);     
-    }
-  }
-  function getSettings() {
-    var storedObject = localStorage[storageKey];
-    var settings;
-    
-    if(storedObject) {
-      settings = JSON.parse(storedObject);
-    } else {
-      // Going for default settings
-      settings = defaultShortcutOptions;
-      saveSettings(settings);
+    function saveSettings(settings) {
+        if(settings) {
+            localStorage[storageKey] = JSON.stringify(settings);     
+        }
     }
 
-    return settings;
-  }
+    function getSettings() {
+        var storedObject = localStorage[storageKey];
+        var settings;
+
+        if(storedObject) {
+            settings = JSON.parse(storedObject);
+        } else {
+            // Going for default settings
+            settings = defaultShortcutOptions;
+            saveSettings(settings);
+        }
+
+        return settings;
+    }
 
 	initialize();
 	
--- cssreloader.content.js
+++ cssreloader.content.js
@@ -1,46 +1,47 @@
 (function() {
 
-  var shortcutSettings;
-	function initialize() {
-    document.addEventListener("keydown", onWindowKeyUp, false);
-    chrome.extension.onRequest.addListener(onExtensionRequest);
-    chrome.extension.sendRequest({'action' : 'getSettings'}, onGetSettings);
-	}
+    var shortcutSettings;
+    
+    function initialize() {
+        document.addEventListener("keydown", onWindowKeyDown, false);
+        chrome.extension.onRequest.addListener(onExtensionRequest);
+        chrome.extension.sendRequest({'action' : 'getSettings'}, onGetSettings);
+    }
 
-	function reload() {
-		var elements = document.querySelectorAll('link[rel=stylesheet][href]');
-		for (var i = 0, element; element = elements[i]; i++) {
-			var href = element.href.replace(/[?&]cssReloader=([^&$]*)/,'');
-			element.href = href + (href.indexOf('?')>=0?'&':'?') + 'cssReloader=' + (new Date().valueOf());
-		}
-	}
+    function reload() {
+        var elements = document.querySelectorAll('link[rel=stylesheet][href]');
+        for (var i = 0, element; element = elements[i]; i++) {
+            var href = element.href.replace(/[?&]cssReloader=([^&$]*)/,'');
+            element.href = href + (href.indexOf('?')>=0?'&':'?') + 'cssReloader=' + (new Date().valueOf());
+        }
+    }
 
-  function onGetSettings(settings) {
-    shortcutSettings = settings;
-  }
+    function onGetSettings(settings) {
+        shortcutSettings = settings;
+    }
 
-	function onWindowKeyUp(e) {
-		if(e.keyIdentifier == shortcutSettings["keyIdentifier"] &&
-       e.shiftKey ===  shortcutSettings["shiftKeySelected"] &&
-       e.altKey === shortcutSettings["altKeySelected"] &&
-       e.ctrlKey === shortcutSettings["controlKeySelected"])
-    {
-			reload();
-		}
-	}
+    function onWindowKeyDown(e) {
+        if(e.keyIdentifier == shortcutSettings["keyIdentifier"] &&
+        e.shiftKey ===  shortcutSettings["shiftKeySelected"] &&
+        e.altKey === shortcutSettings["altKeySelected"] &&
+        e.ctrlKey === shortcutSettings["controlKeySelected"])
+        {
+            reload();
+        }
+    }
 
-	function onExtensionRequest(request, sender) {
-    if (request.action == "reload") {
-		 reload();
-	  }
-	}
+    function onExtensionRequest(request, sender) {
+        if (request.action == "reload") {
+            reload();
+        }
+    }
 
-	CSSreloader = {
-		reload : reload,
-		initialize: initialize
-	};
+    CSSreloader = {
+        reload : reload,
+        initialize: initialize
+    };
 
-	CSSreloader.initialize();
+    CSSreloader.initialize();
 
 })();
 
